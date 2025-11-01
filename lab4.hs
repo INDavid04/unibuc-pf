6e3ba1dfd5061f5661e463f4926656ee2f9b4e88
INDavid04 <davidirimia100@gmail.com>
2025-11-01 12:24:56 +0200

    add: 8-13 to lab4.hs in fifth week at home

--- lab4.hs
+++ lab4.hs
@@ -41,3 +41,40 @@ firstEl list = [x | (x, _) <- list]
 -- Exemplu: sumList [[1,3], [2,4,5], [], [1,3,5,6]] intoarce [4,11,0,15]
 sumList :: [[Int]] -> [Int]
 sumList lists = [sum list | list <- lists]
+
+-- 8. Scrieți o funcție prel2 care are ca parametru o listă de întregi (Int) și întoarce o listă în care elementele pare sunt înjumătățite, iar cele impare sunt dublate:
+-- Exemplu: prel2 [2,4,5,6] intoarce [1,2,10,3]
+
+prel2 :: [Int] -> [Int]
+prel2 list = [ if x `mod` 2 == 0 then x `div` 2 else x * 2 | x <- list ]
+
+-- 9. Scrieți o funcție care primește ca parametri un caracter și o listă de șiruri de caractere, și întoarce lista șirurilor care conțin caracterul primit ca argument (hint: folosiți funcția elem).
+
+listaSirCar :: Char -> [[Char]] -> [[Char]]
+listaSirCar caracter listaSirCaractere = [ sir | sir <- listaSirCaractere, elem caracter sir ]
+
+-- 10. Scrieți o funcție care are ca parametru o listă de întregi și întoarce lista pătratelor numerelor impare din acea listă.
+
+listaPatrateImpare :: [Int] -> [Int]
+listaPatrateImpare lista = [ x * x | x <- lista, x `mod` 2 == 1 ]
+
+-- 11. Scrieți o funcție care primește ca argument o listă de întregi și întoarce lista pătratelor elementelor din poziții impare. Hint: folosiți zip pentru a avea acces la poziția elementelor.
+
+listaPatratePozImpare :: [Int] -> [Int]
+listaPatratePozImpare lista = [ x * x | (i, x) <- zip [0..] lista, i `mod` 2 == 1 ]
+
+-- 12. Scrieți o funcție care primește ca parametru o listă de șiruri de caractere și întoarce lista obținută prin eliminarea consoanelor din fiecare șir.
+-- Exemplu: numaiVocale ["laboratorul", "PrgrAmare", "DEclarativa"] intoarce ["aoaou","Aae","Eaaia"]
+
+numaiVocale :: [[Char]] -> [[Char]]
+numaiVocale liste = [ [ caracter | caracter <- lista, caracter `elem` "aeiouAEIOU" ] | lista <- liste ]
+
+-- 13. Definiți recursiv funcțiile mymap și myfilter cu aceeași funcționalitate ca a funcțiilor map și filter predefinite.
+-- Exemplu: mymap (* 3) [1, 3, 4] intoarce [3, 9, 12]
+-- Exemplu: myfilter (> 2) [3,1,4,2,5] intoarce [3, 4, 5]
+
+mymap :: (a -> b) -> [a] -> [b]
+mymap f xs = [f x | x <- xs]
+
+myfilter :: (a -> Bool) -> [a] -> [a]
+myfilter p xs = [x | x <- xs, p x]
